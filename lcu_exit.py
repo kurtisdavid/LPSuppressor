cb743870ef40904482fe3ab01c432f75b3da8a36
kurtisdavid <kurtis.e.david@gmail.com>
2023-10-13 20:33:10 -0700

    Update dismiss stats to remove vote condition

--- lcu_exit.py
+++ lcu_exit.py
@@ -11,15 +11,14 @@ async def connect(connection):
 # Exit out of post game
 @connector.ws.register('/lol-honor-v2/v1/ballot', event_types=('UPDATE','CREATE','DELETE',))
 async def voted(connection, event):
-    if event.data is None:
-        await connection.request('post', '/lol-end-of-game/v1/state/dismiss-stats')
+    print("Exiting...")
+    await connection.request('post', '/lol-end-of-game/v1/state/dismiss-stats')
 
 # Exit out of Rank Up
 @connector.ws.register('/lol-ranked/v1/notifications')
 async def ranked_notification(connection, event):
     if event.data is not None:
         if isinstance(event.data, list) and len(event.data) > 0:
-            # TODO: Figure out which ID is supposed to be used?
             #  League Client seems to close any notification if invalid (in this case we only have 1)
             await connection.request('post', f'/lol-ranked/v1/notifications/{event.data[0]}/acknowledge')
     
